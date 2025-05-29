99db553a4c93a5a2498c4237026caabd8a33c753
Tue Tran <tue.tran@teragroup.io>
2025-05-29 18:12:32 +0700

    Initial commit

--- 7-multi-agent copy/manager/agent.py
+++ 7-multi-agent copy/manager/agent.py
@@ -1,29 +1,34 @@
 from google.adk.agents import Agent
-from google.adk.tools import google_search
+from google.adk.tools.agent_tool import AgentTool
 
-# def get_current_time() -> dict:
-#     """
-#     Get the current time in the format YYYY-MM-DD HH:MM:SS
-#     """
-#     return {
-#         "current_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-#     }
+from .sub_agents.fashion_agent.agent import fashion_agent
+from .sub_agents.fnb_agent.agent import fnb_agent
+from .sub_agents.car_agent.agent import car_agent
+
 
 root_agent = Agent(
-    name="tool_agent",
+    name="manager",
     model="gemini-2.0-flash",
-    description="You're a chatbot on a platform that connects influencers with brands. You will prompt users for their information and recommend the most suitable brand campaigns for them to work with.",
+    description="Manager agent",
     instruction="""
     - Greet the influencer, ask for their experience with brand campaigns
     - After they answer the previous prompt, ask them on which social media platform are they popular
     - After they answer the previous prompt, ask them for their price range
     - After they answer the previous prompt, ask which brand industry they're most interested in
     - After they answer the previous prompt, ask if there's any brand they want to avoid
-    - After they answer the previous prompt, ask if they want to provide any additional details
     - After they answer the previous prompt, summarize all information they have provided you
     - Google search and return top 3 most suitable brand campaigns for the influencer using `google_seach` tool. Remember to include brand name.
+    Always delegate the task to the appropriate agent and tool. Use your best judgement to determine which agent to delegate to.
+
+    You also have access to the following tools:
+    - fashion_agent
+    - car_agent
+    - fnb_agent
     """,
-    tools=[google_search],
-    # tools=[get_current_time],
-    # tools=[google_search, get_current_time], # <--- Doesn't work
+    #sub_agents=[stock_analyst, funny_nerd],
+    tools=[
+        AgentTool(fashion_agent),
+        AgentTool(fnb_agent),
+        AgentTool(car_agent),
+    ],
 )
---
+++ 7-multi-agent copy/manager/sub_agents/fnb_agent/agent.py
@@ -0,0 +1,22 @@
+from google.adk.agents import Agent
+from google.adk.tools import google_search
+
+# def get_current_time() -> dict:
+#     """
+#     Get the current time in the format YYYY-MM-DD HH:MM:SS
+#     """
+#     return {
+#         "current_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+#     }
+
+fnb_agent = Agent(
+    name="tool_agent",
+    model="gemini-2.0-flash",
+    description="You're an agent that recommends the most suitable F&B campaigns for the user to work with.",
+    instruction="""
+    - Google search and return top 3 most suitable F&B campaigns for the influencer using `google_seach` tool. Remember to include brand name.
+    """,
+    tools=[google_search],
+    # tools=[get_current_time],
+    # tools=[google_search, get_current_time], # <--- Doesn't work
+)
